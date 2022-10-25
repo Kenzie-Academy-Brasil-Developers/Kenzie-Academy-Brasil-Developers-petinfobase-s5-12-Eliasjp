1b9809b35af748eb1565e95c87fdef2d41c4b95a
eliasjp <illumine1998@gmail.com>
2022-10-25 20:48:38 -0300

    fix html

--- pages/login/login-localStorage.js
+++ pages/login/login-localStorage.js
@@ -2,7 +2,7 @@ import { callLoginStorage } from "../../scripts/localStorage.js"
 
 function checkAccount (){
     if (callLoginStorage()){
-        window.location.assign("/pages/home/home.html", "_self")
+        window.location.assign("../../pages/home/home.html", "_self")
     }
 }
 
